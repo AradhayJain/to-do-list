aa478710bd1670d15f82b209739cc99d05e2de39
AradhayJain <153743476+AradhayJain@users.noreply.github.com>
2024-01-07 22:41:28 +0530

    added index.html

---
+++ index.js
@@ -0,0 +1,22 @@
+let inputs=document.getElementById('inp');
+let text=document.querySelector('.TEXT');
+
+function ADD(){
+
+    if (inputs.value==""){
+        alert('please enter a task')
+    }
+    else{
+        let newEle=document.createElement('ul');
+        newEle.innerHTML=`${inputs.value}<i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        inputs.value="";
+    
+
+        newEle.querySelector('i').addEventListener("click",remove);
+
+        function remove(){
+            newEle.remove()
+        }
+    }
+}
